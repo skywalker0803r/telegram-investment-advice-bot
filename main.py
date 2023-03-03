e3c1a6e15a36c54d9608fbf614d9f86e7b4a414c
skywalker0803r <skywalker0803r@gmail.com>
2023-03-04 01:07:56 +0800

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#https://sharegpt.com/c/kZOYTX1
 import yfinance as yf
 from finta import TA
 from keras.models import load_model
@@ -9,8 +10,9 @@ import time
 from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
-#https://sharegpt.com/c/kZOYTX1
 
+
+# 發電報函數
 def send_to_telegram(message):
     apiToken = '5850662274:AAGeKZqM1JfQfh3CrSKG6BZ9pEvDajdBUqs'
     chatID = '1567262377'
@@ -21,29 +23,32 @@ def send_to_telegram(message):
     except Exception as e:
         print(e)
 
+# 計算金融技術指標函數
+def calculate_ta(df):
+    ta_functions = [TA.RSI, TA.WILLIAMS, TA.SMA, TA.EMA, TA.WMA, TA.HMA, TA.TEMA, TA.CCI, TA.CMO, TA.MACD, TA.PPO, TA.ROC, TA.CFI, TA.DMI, TA.SAR]
+    ta_names = ['RSI', 'Williams %R', 'SMA', 'EMA', 'WMA', 'HMA', 'TEMA', 'CCI', 'CMO', 'MACD', 'PPO', 'ROC', 'CFI', 'DMI', 'SAR']
+    for i, ta_func in enumerate(ta_functions):
+        try:
+            df[ta_names[i]] = ta_func(df)
+        except:
+            if ta_names[i] == 'MACD':
+                df[ta_names[i]] = ta_func(df)['MACD']-ta_func(df)['SIGNAL']
+            if ta_names[i] == 'PPO':
+                df[ta_names[i]] = ta_func(df)['PPO']-ta_func(df)['SIGNAL']  
+            if ta_names[i] == 'DMI':
+                df[ta_names[i]] = ta_func(df)['DI+']-ta_func(df)['DI-']
+    return df
+
+# 給定歷史數據,scaler,model,輸出買賣訊號的函數
 def process(df,min_max_scaler,model):
-    df['RSI'] = TA.RSI(df)#1
-    df['Williams %R'] = TA.WILLIAMS(df)#2
-    df['SMA'] = TA.SMA(df)#3
-    df['EMA'] = TA.EMA(df)#4
-    df['WMA'] = TA.WMA(df)#5
-    df['HMA'] = TA.HMA(df)#6
-    df['TEMA'] = TA.TEMA(df)#7
-    df['CCI'] = TA.CCI(df)#8
-    df['CMO'] = TA.CMO(df)#9
-    df['MACD'] = TA.MACD(df)['MACD'] - TA.MACD(df)['SIGNAL']#10
-    df['PPO'] = TA.PPO(df)['PPO'] - TA.PPO(df)['SIGNAL']#11
-    df['ROC'] = TA.ROC(df)#12
-    df['CFI'] = TA.CFI(df)#13
-    df['DMI'] = TA.DMI(df)['DI+'] - TA.DMI(df)['DI-']#14
-    df['SAR'] = TA.SAR(df)#15
+    df = calculate_ta(df)
     df = df.dropna(axis=0)
-    features = df.columns[-15:].tolist()
+    features = ['RSI', 'Williams %R', 'SMA', 'EMA', 'WMA', 'HMA', 'TEMA', 'CCI', 'CMO', 'MACD', 'PPO', 'ROC', 'CFI', 'DMI', 'SAR']
     Close = df[['Close']]
     df = df[features]
-    # 數值轉換
+    # 數據轉換
     df[features] = min_max_scaler.transform(df[features])
-    # 製作X
+    # 製作X(15x15's array)
     days = 15
     start_index = 0
     end_index = len(df)-days
@@ -55,42 +60,52 @@ def process(df,min_max_scaler,model):
         Xs.append(X)
         indexs.append((df.iloc[[i]].index,df.iloc[[i+days-1]].index))
     Xs = np.array(Xs)
-    # 模型預測
-    answer = model.predict(Xs)
-    answer = [ np.argmax(i) for i in answer]
+    # 模型預測買賣訊號
+    singal = model.predict(Xs)
+    singal = [ np.argmax(i) for i in singal]
     
     # 繪圖
     Close = Close.iloc[-len(Xs):,:]
-    Close['SIGNAL'] = answer
+    Close['SIGNAL'] = singal
     buy = Close[Close['SIGNAL']==1]['Close']
     sell = Close[Close['SIGNAL']==2]['Close']
+    
     Close['Close'].plot()
     plt.scatter(list(buy.index),list(buy.values),color='red',marker="^")
+    plt.show()
+    
+    Close['Close'].plot()
     plt.scatter(list(sell.index),list(sell.values),color='green',marker='v')
     plt.show()
     
-    return answer[-1]
+    return singal[-1]
 
 def main():
+    # 載入模型和scaler
     model = load_model('model.h5')
     with open('scaler.pkl', 'rb') as f:
         min_max_scaler = pickle.load(f)
+    # 資料時間範圍設定
     start_date = (dt.datetime.now() - dt.timedelta(days=180)).strftime("%Y-%m-%d")
     end_date = dt.datetime.now().strftime("%Y-%m-%d")
-    df = yf.download('^TWII', start=start_date, end=end_date)
+    symbol = '^TWII'
+    df = yf.download(symbol, start=start_date, end=end_date)
+    # 取得交易訊號
     signal = process(df,min_max_scaler,model)
     if signal == 0:
-        send_to_telegram('不動作')
+        action = '不動作'
     if signal == 1:
-        send_to_telegram('買進')
+        action = '建議買入'
     if signal == 2:
-        send_to_telegram('賣出')
+        action = '建議賣出'
+    # 發送消息至telegram
+    current_time = dt.datetime.now().strftime("%Y-%m-%d")
+    message = f"現在時間:{current_time} 投資項目:{symbol} 當前建議操作:{action}"
+    send_to_telegram(message)
 
 if __name__ == '__main__':
     main()
-    '''
-    schedule.every().day.at('08:00').do(main)
+    schedule.every().day.at('08:00').do(main)#每日八點準時執行
     while True:
         schedule.run_pending()
         time.sleep(1)
-    '''
